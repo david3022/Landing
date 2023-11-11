6eff8fc9c966a1b8f4192434e8d026d85bdccb93
David3022 <davidafs36@gmail.com>
2023-11-11 03:18:56 -0400

    Update Works.jsx
    Agregada la redirección a los demas proyectos en la carpeta works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -18,27 +19,35 @@ const ProjectCard = ({
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <div className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full relative overflow-hidden'>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+      >
         <div
           onClick={() => window.open(source_code_link2, "_blank")}
-          className='relative w-full h-[230px] overflow-hidden rounded-2xl'
-        >
-          <img
+          >
+          <div className='relative w-full h-[230px]'>
+           <img
             src={image}
             alt='project_image'
-            className='w-full h-full object-cover rounded-2xl transition-transform transform hover:scale-105'
+            className='w-full h-full object-cover rounded-2xl'
           />
         </div>
-        <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
-          <div
-            onClick={() => window.open(source_code_link, "_blank")}
-            className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
-          >
-            <img
-              src={github}
-              alt='source code'
-              className='w-1/2 h-1/2 object-contain'
-            />
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img
+                src={github}
+                alt='source code'
+                className='w-1/2 h-1/2 object-contain'
+              />
+            </div>
           </div>
         </div>
 
@@ -57,7 +66,7 @@ const ProjectCard = ({
             </p>
           ))}
         </div>
-      </div>
+      </Tilt>
     </motion.div>
   );
 };
@@ -75,7 +84,7 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
-          Following projects showcase my skills and experience through
+          Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
           links to code repositories and live demos in it. It reflects my
           ability to solve complex problems, work with different technologies,
